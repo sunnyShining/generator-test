cb1bb5efebfe812770c4bd59193127191cd1f700
sunny <jdhydca@icloud.com>
2017-12-03 12:05:47 +0800

    first commit

---
+++ generators/app/templates/Feed/Feed.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import { Icon } from 'antd';
+import PropTypes from 'prop-types';
+import './<%- routesName %>.less';
+
+export default class <%- routesName %> extends Component {
+	render() {
+		return (
+  			<div><%- routesName %></div>
+		);
+	}
+}
+
+<%- routesName %>.propTypes = {
+};
